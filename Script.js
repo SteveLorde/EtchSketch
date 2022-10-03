cb96b1ff770b854cac4cd740779798e859ec7762
SteveLorde <mostafa.maher98@gmail.com>
2022-10-03 11:02:25 +0200

    Layout 3

--- Script.js
+++ Script.js
@@ -1,11 +1,18 @@
 
-const ScreenGrid=document.querySelector('.Screen')
-
-
 //1-Function to Create Grid
-function CreateGrid(x) {
-    rows=0
-    columns=0
+function PixelCanvasCreate() {
+    let ScreenCanvas=document.getElementsByClassName('Screen')
 
+    for (let i = 0; i < 11; i++) {
+        let myRow = document.createElement('div')
+        myRow.id = 'row' + i
+
+        ScreenCanvas.appendChild(myRow)
+
+        
+    }
     
 }
+
+BTNGRIDNEW = document.getElementsByClassName('buttonclear')
+BTNGRIDNEW.onclick = PixelCanvasCreate()
